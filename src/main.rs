990567c0e4c22793dd070520d99751b787a6d361
Jean-Marc Alliot <jm@alliot.org>
2023-08-14 16:56:58 +0200

    Cosmetic changes

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use pathfinding::prelude::astar;
 use cpu_time::ProcessTime;
 
 const DIM:usize=3;
-#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+
+#[repr(packed)]
+#[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 struct MyPos {
     mat: [[u8;DIM];DIM],
     i: u8,
-    j:u8
+    j: u8
 }
 
 impl MyPos {
@@ -14,14 +16,14 @@ impl MyPos {
 	let mut sum:i32 = 0;
 	for (i,v) in self.mat.iter().enumerate() {
 	    for (j,x) in v.iter().enumerate() {
-                if *x!=0u8 {
+                if *x!=0_u8 {
 		    let i1:i32=((*x as i32)-1) / (DIM as i32);
 		    let j1:i32=((*x as i32)-1) % (DIM as i32);
 		    sum+=(i1-(i as i32)).abs()+(j1-(j as i32)).abs();
                 }
 	    }
 	}
-	return sum as u32;
+	sum as u32
     }
     fn mysuccessors(&self) -> Vec<(Self, u32)> {
 	let mut vec = Vec::new();
@@ -34,7 +36,7 @@ impl MyPos {
 		nv.mat[self.i as usize][self.j as usize]=nv.mat[ni][nj];
 		nv.mat[ni][nj]=0;
 		nv.i=ni as u8;
-		vec.push((nv,1 as u32));
+		vec.push((nv,1_u32));
             }
         }
 	for j in [-1,1] {
@@ -46,11 +48,10 @@ impl MyPos {
 		nv.mat[self.i as usize][self.j as usize]=nv.mat[ni][nj];
 		nv.mat[ni][nj]=0;
 		nv.j=nj as u8;
-		vec.push((nv,1 as u32));
+		vec.push((nv,1_u32));
 	    }
 	}
-//        println!("{:?}",vec);
-	return vec;
+	vec
     }
     fn normalize(&mut self) {
         for (i,v) in self.mat.iter().enumerate() {
@@ -67,7 +68,7 @@ impl MyPos {
 fn build(v:Vec<usize>,n:usize,m:&mut MyPos,t:&mut Vec<u32>) {
     for (k,e) in v.iter().enumerate() {
         let mut v2 = v.clone();
-        let r = v2.remove(k);
+        let _r = v2.remove(k);
         let i=n/DIM;
         let j=n%DIM;
         m.mat[i][j]= *e as u8;
@@ -79,11 +80,11 @@ fn build(v:Vec<usize>,n:usize,m:&mut MyPos,t:&mut Vec<u32>) {
                 |p| p.mysuccessors(),
                 |p| p.mydistance(),
 	        |p| p.mydistance()==0) {
-                println!("{:?} {:?} {}",m,spt.elapsed(),len);
+                println!("{:?} {:?} {}",m.mat,spt.elapsed(),len);
                 t[len as usize]+=1;
             }
             else {
-                println!("{:?} {:?}",m,spt.elapsed());
+                println!("{:?} {:?}",m.mat,spt.elapsed());
             }
         }
         else {
@@ -93,14 +94,9 @@ fn build(v:Vec<usize>,n:usize,m:&mut MyPos,t:&mut Vec<u32>) {
 }
 
 fn main() {
-    let mut tlen:Vec<u32> = Vec::new();
-    tlen.resize(40,0);
-     let mut zero: MyPos = MyPos {
-        mat: [[0,0,0],[0,0,0],[0,0,0]],
-        i:0,
-        j:0
-    };
-    let v = [0,1,2,3,4,5,6,7,8];
-    build(v.to_vec(),0,&mut zero,&mut tlen);
+    let mut tlen = vec![0_u32;40];
+    let mut zero: MyPos = Default::default();
+    let v: Vec<usize> = Vec::from_iter(0..DIM*DIM);
+    build(v,0,&mut zero,&mut tlen);
     println!("{:?}",tlen);
 }
